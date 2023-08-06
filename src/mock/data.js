9f05a49a0b632629faf76e7a30374f97afeadbe5
junaedi <desistrom0@gmail.com>
2023-08-06 14:46:43 +0700

    first commit

---
+++ src/mock/data.js
@@ -0,0 +1,64 @@
+export default {
+  code: 
+`
+// good mood today
+// I'll show you the code
+// I'm going to start typing
+const { 😆, 🤗 } = 🌎;
+😆.say('marry me!❤️');
+🤗.response('ok, as long as you buy me🍉！');
+Utils.marry(😆,🤗);
+Utils.generateInvitation(😆,🤗);
+// ok i'm done
+// I'm about to start running`,
+  executions: [{
+    name:'initialization',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Unpacking',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Build',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Packing',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Smile🙂',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Laught😄',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'compilation...',
+    time: '',
+    visible: false
+  }],
+  barrages: [
+    'wkwkwkwk',
+    'semoga impianmu menjadi kenyataan',
+    'kamu yg terbaik',
+    'undanganmu sangat indah',
+    'samawa terus yaaa',
+    'weh weh weh',
+    'aku padamu., wkwkwkwk',
+    'gas maszeee',
+    'Semoga bahagia selalu',
+    'keren gan',
+    'weh, heckelll',
+    '😗 😙 😚 😋 ',
+    'mangan mangan'
+  ],
+}
